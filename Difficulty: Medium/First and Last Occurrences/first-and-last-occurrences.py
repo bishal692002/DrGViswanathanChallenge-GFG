f766332a42762ecef9eb18054933188bbdb97f56
Bishal Biswas <105965588+bishal692002@users.noreply.github.com>
2025-05-09 23:36:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+
+class Solution:
+    def find(self, arr, x):
+        first = 0
+        last = 0
+        found = False 
+
+        for i in range(len(arr)):
+            if arr[i] == x:
+                if not found:
+                    first = i
+                    found = True
+                last = i
+
+        if not found:
+            return [-1, -1]  
+        return [first, last]
+
+
+#{ 
+ # Driver Code Starts
+t = int(input())  # Number of test cases
+for _ in range(t):
+    arr = list(map(int, input().split()))  # Input array for each test case
+    x = int(input())  # Element to search for
+    ob = Solution()
+    ans = ob.find(arr, x)  # Call the find function in the Solution class
+    print(*ans)  # Print the result as space-separated values
+    print("~")
+
+# } Driver Code Ends
