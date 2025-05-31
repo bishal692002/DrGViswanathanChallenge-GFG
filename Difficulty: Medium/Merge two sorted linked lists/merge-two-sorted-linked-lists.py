07dbc58bf3c753cb66d6a02a562655be2ae95b84
Bishal Biswas <105965588+bishal692002@users.noreply.github.com>
2025-05-31 14:34:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.py
@@ -0,0 +1,11 @@
+'''
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+'''
+class Solution:
+    def sortedMerge(self,head1, head2):
+        # code here
