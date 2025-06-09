3cfadc7722e9b520b509384a726cfdf95a297b5e
Bishal Biswas <105965588+bishal692002@users.noreply.github.com>
2025-06-09 23:37:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxLen(self, arr):
+        mx=0
+        sm=0
+        seen={0:-1}
+        for ix,ve in enumerate(arr):
+            sm+=ve
+            if sm in seen:
+                mx=max(mx,ix-seen[sm])
+            if sm not in seen:
+                seen[sm]=ix
+        return mx
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        print(ob.maxLen(arr))
+        print("~")
+
+# } Driver Code Ends
