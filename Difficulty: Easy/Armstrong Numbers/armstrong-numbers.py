d12321b19b6a01308e1322f37ea56b0a665b4a8c
Bishal Biswas <105965588+bishal692002@users.noreply.github.com>
2025-06-13 22:28:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+
+class Solution:
+    def armstrongNumber (self, n):
+        # code here
+        temp = n
+        length = len(str(n)) 
+        result = 0
+        while(n != 0):
+            digit  = n % 10
+            result = result + digit ** length
+            n = n // 10
+        if result == temp:
+            return True
+        else:
+            return False
