bca272f190893695f76e3f8871b0cb3d7156a56e
Bishal Biswas <105965588+bishal692002@users.noreply.github.com>
2025-07-07 14:19:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Selection Sort/selection-sort.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+
+class Solution: 
+    def selectionSort(self, arr):
+        for i in range(len(arr)):
+            mini = i
+            for j in range(i+1, len(arr)):
+                if arr[mini]> arr[j]:
+                    mini = j
+            arr[i], arr[mini] = arr[mini],arr[i]
+        return arr
+
+
+
+
+
