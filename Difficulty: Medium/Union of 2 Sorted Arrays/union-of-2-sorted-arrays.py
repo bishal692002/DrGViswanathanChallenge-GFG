6213055927d9a4d3b1feb571119df9f93b25be7f
Bishal Biswas <105965588+bishal692002@users.noreply.github.com>
2025-07-11 23:29:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.py
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.py
@@ -1,3 +1,38 @@
 class Solution:
-   def findUnion(self,a,b):
-      return sorted(set(a+b))
+    
+    #Function to return a list containing the union of the two arrays.
+    def findUnion(self,a,b):
+        # code here 
+        s=[]
+        seen=set()
+        l1=len(a)
+        l2=len(b)
+        i=0
+        j=0
+        while i<l1 and j<l2:
+            if a[i]<b[j]:
+                temp=a[i]
+                if temp not in seen:
+                    s.append(temp)
+                    seen.add(temp)
+                i+=1
+            else:
+                temp=b[j]
+                if temp not in seen:
+                    s.append(temp)
+                    seen.add(temp)
+                j+=1
+        while i<l1:
+            temp=a[i]
+            if temp not in seen:
+                s.append(temp)
+                seen.add(temp)
+            i+=1
+        while j<l2:
+            temp=b[j]
+            if temp not in seen:
+                s.append(temp)
+                seen.add(temp)
+            j+=1
+            
+        return s
