24d55a7f5b097276fcafab5469d59293aa39b158
Bishal Biswas <105965588+bishal692002@users.noreply.github.com>
2025-07-14 00:01:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.py
@@ -0,0 +1,11 @@
+class Solution:
+    def romanToDecimal(self, s): 
+        # code here
+        d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        c=0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for i in s:
+            c=c+d[i]
+        return c
