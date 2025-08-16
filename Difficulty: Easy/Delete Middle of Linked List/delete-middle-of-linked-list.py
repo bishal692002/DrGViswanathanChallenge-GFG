a3f2ed436edd4363556aed36054dbf72bee7516c
Bishal Biswas <105965588+bishal692002@users.noreply.github.com>
2025-08-17 00:02:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Middle of Linked List/delete-middle-of-linked-list.py
@@ -0,0 +1,27 @@
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+
+
+class Solution:
+
+    def deleteMid(self, head):
+        '''
+        head:  head of given linkedList
+        return: head of resultant llist
+        '''
+
+        #code here
+        if head.next is None:
+            return None
+        slow=fast=prev = head
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        prev.next = slow.next
+        slow.next = None
+        return head
